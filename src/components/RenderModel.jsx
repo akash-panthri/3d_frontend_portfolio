8e93aae53a8ed1c299609a824ed8a17489c962d8
akash-panthri <akashpanthri2000@gmail.com>
2024-11-19 07:21:51 +0530

    Added code to RenderModel.jsx

---
+++ src/components/RenderModel.jsx
@@ -0,0 +1,18 @@
+import React, { Suspense } from 'react'
+import { Canvas } from "@react-three/fiber";
+import clsx from "clsx";
+
+function RenderModel({children, className}) {
+  return (
+    <Canvas
+    className={clsx("w-screen h-screen relative", className)}
+    shadows={false}
+    dpr={[1, 2]}
+    
+  >
+    <Suspense fallback={null}>{children}</Suspense>
+  </Canvas>
+  )
+}
+
+export default RenderModel
