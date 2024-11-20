bf07adcefc65241221b75bb978d7adbfecd67bcd
akash-panthri <akashpanthri2000@gmail.com>
2024-11-20 07:26:53 +0530

    Rendered Wizard

--- src/components/RenderModel.jsx
+++ src/components/RenderModel.jsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { Suspense } from 'react'
 import { Canvas } from "@react-three/fiber";
 import clsx from "clsx";
@@ -5,7 +6,7 @@ import clsx from "clsx";
 function RenderModel({children, className}) {
   return (
     <Canvas
-    className={clsx("w-screen h-screen relative", className)}
+    className={clsx("w-screen h-screen -z-10 relative", className)}
     shadows={false}
     dpr={[1, 2]}
     
