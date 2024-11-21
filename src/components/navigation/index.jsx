95ee7c11a0b0996454ec9fb9c71928a8f9131964
akash-panthri <akashpanthri2000@gmail.com>
2024-11-21 07:50:58 +0530

    Rendered circular buttons

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -1,16 +1,23 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
 import { BtnList } from "@/app/data";
 
 export default function Navigation() {
+  const angleIncrement = 360 / BtnList.length;
   return (
-    <div className='flex items-center justify-between relative'>
-    
-    {BtnList.map((btn, index) => {
-                
+    <div className="w-full fixed h-screen flex items-center justify-center">
 
-                return <button key={index}>{btn.label}</button>
-              })}
+   
+    <div className="flex items-center justify-between relative">
+      {BtnList.map((btn, index) => {
+        const angleRad = (index * angleIncrement * Math.PI) / 180;
+
+        const radius = "calc(20vw - 1rem)";
+        const x = `calc(${radius}*${Math.cos(angleRad)})`;
+        const y = `calc(${radius}*${Math.sin(angleRad)})`;
+        return <button key={index} className="absolute" style={{transform:`translate(${x}, ${y})`}}>{btn.label}</button>;
+      })}
     </div>
-  )
+    </div>
+  );
 }
