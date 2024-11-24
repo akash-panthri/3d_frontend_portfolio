1a80297d4c2412d65c2d08343604f619c193c47f
akash-panthri <akashpanthri2000@gmail.com>
2024-11-24 07:39:30 +0530

    Added animation in tailwind.config.js

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -9,7 +9,7 @@ export default function Navigation() {
     <div className="w-full fixed h-screen flex items-center justify-center">
 
    
-    <div className="flex items-center justify-between relative">
+    <div className="w-max flex items-center justify-between relative animate-spin-slow">
       {BtnList.map((btn, index) => {
         const angleRad = (index * angleIncrement * Math.PI) / 180;
 
