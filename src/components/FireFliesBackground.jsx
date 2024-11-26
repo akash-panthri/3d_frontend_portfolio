8a4e91c7fdff6893736743d9cfa06fedbc8328d5
akash-panthri <akashpanthri2000@gmail.com>
2024-11-26 07:27:04 +0530

    Added createFirefly function in FireFliesBackground.jsx

--- src/components/FireFliesBackground.jsx
+++ src/components/FireFliesBackground.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
+
+const createFirefly = () => ({
+    id: Math.random(),
+    top: `${Math.random() * 100}%`,
+    left: `${Math.random() * 100}%`,
+    animationDuration: `${Math.random() * 5 + 5}s`,
+  });
+
+  
 function FireFliesBackground() {
   return (
     <div className="fixed top-0 left-0 w-full h-full -z-10 overflow-hidden">
