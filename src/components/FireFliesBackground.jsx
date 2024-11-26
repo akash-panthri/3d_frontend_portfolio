d24322fb1fe3c5b9346d058216c5b451c2abd415
akash-panthri <akashpanthri2000@gmail.com>
2024-11-26 07:50:42 +0530

    Finished firefly effect

--- src/components/FireFliesBackground.jsx
+++ src/components/FireFliesBackground.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client"
+import React, {useEffect, useState } from "react";
 
 
 const createFirefly = () => ({
@@ -8,11 +9,42 @@ const createFirefly = () => ({
     animationDuration: `${Math.random() * 5 + 5}s`,
   });
 
-  
+
 function FireFliesBackground() {
+    const [fireflies, setFireflies] = useState([]);
+console.log("fireflies")
+console.log(fireflies)
+console.log("firefliesdfas")
+    useEffect(() => {
+        const addFireflyPeriodically = () => {
+          const newFirefly = createFirefly();
+          setFireflies((currentFireflies) => [
+            ...currentFireflies.slice(-14),
+            newFirefly,
+          ]);
+        };
+    
+        const interval = setInterval(addFireflyPeriodically, 1000);
+    
+        return () => clearInterval(interval);
+      }, []);
+
+      
   return (
     <div className="fixed top-0 left-0 w-full h-full -z-10 overflow-hidden">
-      FireFliesBackground
+      {fireflies.map((firefly) => {
+        return (
+          <div
+            key={firefly.id}
+            className="absolute rounded-full w-[10px] h-[10px] bg-firefly-radial"
+            style={{
+              top: firefly.top,
+              left: firefly.left,
+              animation: `move ${firefly.animationDuration} infinite alternate`,
+            }}
+          ></div>
+        );
+      })}
     </div>
   );
 }
