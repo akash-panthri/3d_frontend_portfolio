462b01879a21186c7489468316c294a1ea026b43
akash-panthri <akashpanthri2000@gmail.com>
2024-11-26 07:05:33 +0530

    Added FireFliesBackground.jsx

---
+++ src/components/FireFliesBackground.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FireFliesBackground() {
+  return (
+    <div className="fixed top-0 left-0 w-full h-full -z-10 overflow-hidden">
+      FireFliesBackground
+    </div>
+  );
+}
+
+export default FireFliesBackground;
