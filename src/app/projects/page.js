e17c6981bceb2cff99fd0298345efaa385b4b90d
akash-panthri <akashpanthri2000@gmail.com>
2024-11-27 07:08:22 +0530

    Added Project folder and basic code

---
+++ src/app/projects/page.js
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import bg from "../../../public/background/projects-background.png";
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between relative">
+      <Image
+        src={bg}
+        fill
+        alt="background-image"
+        className="-z-50 w-full h-full object-cover object-center opacity-25"
+      />
+      
+    </main>
+  );
+}
