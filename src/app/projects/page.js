6e16a11d9d021618232a4fb3c1834a200cd04bca
akash-panthri <akashpanthri2000@gmail.com>
2024-11-29 07:08:46 +0530

    Added rendermodel to app/projects/page.jsx

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import bg from "../../../public/background/projects-background.png";
 import ProjectList from "@/components/projects";
+import Wizard from "@/components/models/Wizard";
+import RenderModel from "@/components/RenderModel";
 import { projectsData } from "../data";
 
 
@@ -14,6 +16,10 @@ export default function Home() {
         className="-z-50 w-full h-full object-cover object-center opacity-25"
       />
       <ProjectList projects={projectsData} />
+
+      <RenderModel>
+          <Wizard />
+        </RenderModel>
     </main>
   );
 }
