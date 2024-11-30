949ce9f93d93de26697821ce33d7420b31e22fb3
akash-panthri <akashpanthri2000@gmail.com>
2024-11-30 07:50:43 +0530

    Added HomeBtn.jsx

--- src/app/(sub-folder)/projects/page.js
+++ src/app/(sub-folder)/projects/page.js
@@ -5,23 +5,20 @@ import Staff from "@/components/models/Staff";
 import RenderModel from "@/components/RenderModel";
 import { projectsData } from "../../data";
 
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between relative">
+    <>
       <Image
         src={bg}
-        fill
         alt="background-image"
-        className="-z-50 w-full h-full object-cover object-center opacity-25"
+        className="-z-50 w-full h-full object-cover object-center opacity-25 fixed top-20 left-0"
       />
       <ProjectList projects={projectsData} />
-<div className="flex items-center justify-center fixed  top-20 left-0 h-screen">
-
-      <RenderModel>
+      <div className="flex items-center justify-center fixed  top-20 left-0 h-screen">
+        <RenderModel>
           <Staff />
         </RenderModel>
-</div>
-    </main>
+      </div>
+    </>
   );
 }
