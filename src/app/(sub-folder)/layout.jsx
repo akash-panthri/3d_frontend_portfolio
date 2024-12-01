40f414ecc1f975773cc7390bbe82de833ebfc42f
akash-panthri <akashpanthri2000@gmail.com>
2024-12-01 07:22:44 +0530

    Styled HomeBtn.jsx

--- src/app/(sub-folder)/layout.jsx
+++ src/app/(sub-folder)/layout.jsx
@@ -1,8 +1,10 @@
+import HomeBtn from "@/components/HomeBtn";
 
 
 export default function SubPagesLayout({ children }) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center px-16 py-20">
+    <HomeBtn />
       {children}
     </main>
   );
--- src/components/HomeBtn.jsx
+++ src/components/HomeBtn.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Link from "next/link";
+import { Home } from 'lucide-react';
 
 function HomeBtn() {
   return (
-    <div>HomeBtn</div>
+    <Link
+        href={"/"}
+        target={"_self"}
+        className="text-foreground  rounded-full flex items-center justify-center custom-bg fixed top-4 left-4 w-fit self-start"
+        aria-label={"home"}
+        name={"home"}
+      >
+        <span className="relative  w-14 h-14 p-4  hover:text-accent">
+        <Home className="w-full h-auto" strokeWidth={1.5} />
+          <span className="peer bg-transparent absolute top-0 left-0 w-full h-full" />
+          <span className="absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 bg-background text-foreground text-sm rounded-md shadow-lg whitespace-nowrap">
+            Home
+          </span>
+        </span>
+      </Link>
   )
 }
 
