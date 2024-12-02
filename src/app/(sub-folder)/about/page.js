e18b451dcd9ac85ea3fdb93142d8ab5518dc07d2
akash-panthri <akashpanthri2000@gmail.com>
2024-12-02 07:30:27 +0530

    Added HatModel.jsx

--- src/app/(sub-folder)/about/page.js
+++ src/app/(sub-folder)/about/page.js
@@ -1,9 +1,7 @@
 import Image from "next/image";
 import bg from "../../../../public/background/about-background.png";
-import ProjectList from "@/components/projects";
 import Staff from "@/components/models/Staff";
 import RenderModel from "@/components/RenderModel";
-import { projectsData } from "../../data";
 
 export default function Home() {
   return (
@@ -13,7 +11,7 @@ export default function Home() {
         alt="background-image"
         className="-z-50 w-full h-full object-cover object-center opacity-25 fixed top-20 left-0"
       />
-      <ProjectList projects={projectsData} />
+     
       <div className="flex items-center justify-center fixed  top-20 -left-24 h-screen">
         <RenderModel>
           <Staff />
