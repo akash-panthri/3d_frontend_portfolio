8ece43cc2c84c01b857c335f0e9dd8f7da85cbe0
akash-panthri <akashpanthri2000@gmail.com>
2024-12-02 07:48:09 +0530

    Added code to HatModel.jsx

--- src/app/(sub-folder)/about/page.js
+++ src/app/(sub-folder)/about/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import bg from "../../../../public/background/about-background.png";
-import Staff from "@/components/models/Staff";
 import RenderModel from "@/components/RenderModel";
+import HatModel from "@/components/models/HatModel";
 
 export default function Home() {
   return (
@@ -14,7 +14,7 @@ export default function Home() {
      
       <div className="flex items-center justify-center fixed  top-20 -left-24 h-screen">
         <RenderModel>
-          <Staff />
+          <HatModel />
         </RenderModel>
       </div>
     </>
