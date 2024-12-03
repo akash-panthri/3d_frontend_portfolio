5f0b016262d84b494e40793e8bee843febef6eef
akash-panthri <akashpanthri2000@gmail.com>
2024-12-03 07:06:51 +0530

    Added name and text to about

--- src/app/(sub-folder)/about/page.js
+++ src/app/(sub-folder)/about/page.js
@@ -9,13 +9,21 @@ export default function Home() {
       <Image
         src={bg}
         alt="background-image"
-        className="-z-50 w-full h-full object-cover object-center opacity-25 fixed top-20 left-0"
+        className="-z-50 w-full h-full object-cover object-center opacity-25 fixed top-0 left-0"
       />
-     
-      <div className="flex items-center justify-center fixed  top-20 -left-24 h-screen">
+
+      <div className="w-full h-screen absolute top-1/2 -translate-y-1/2 left-0 z-10">
         <RenderModel>
           <HatModel />
         </RenderModel>
+      </div>
+      <div className="relative w-full h-screen flex flex-col items-center justify-center"> 
+      <div className="absolute flex flex-col items-center text-center top-[60%] left-1/2 -translate-y-1/2 -translate-x-1/2">
+        <h1 className="font-bold text-9xl text-accent" >Akash&nbsp;Panthri</h1>
+        <p className="font-light text-foreground text-lg">
+          Meet the wizard behind this portfolio
+        </p>
+      </div>
       </div>
     </>
   );
