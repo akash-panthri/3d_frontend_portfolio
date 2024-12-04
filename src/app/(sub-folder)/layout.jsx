0b2a9b48ce51179dbaeefba29cc825ca9cbb3316
akash-panthri <akashpanthri2000@gmail.com>
2024-12-04 07:59:04 +0530

    Used ItemLayout element in index.js in components/about

--- src/app/(sub-folder)/layout.jsx
+++ src/app/(sub-folder)/layout.jsx
@@ -3,7 +3,7 @@ import HomeBtn from "@/components/HomeBtn";
 
 export default function SubPagesLayout({ children }) {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center px-16 py-20">
+    <main className="flex min-h-screen flex-col items-center justify-center px-32 py-20">
     <HomeBtn />
       {children}
     </main>
