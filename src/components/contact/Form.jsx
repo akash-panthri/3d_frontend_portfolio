cf6abf0b32870e7616a681937724539d758df125
akash-panthri <akashpanthri2000@gmail.com>
2024-12-08 07:04:09 +0530

    Added basic form code to form.jsx

--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+"use client"
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
-function Form() {
+export default function Form() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+  console.log(errors);
+  
   return (
-    <div>Form</div>
-  )
-}
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" placeholder="name" {...register("name", {required: true})} />
+      <input type="email" placeholder="email" {...register("email", {required: true})} />
+      <textarea {...register("message", {required: true, max: 256, min: 50})} />
 
-export default Form
+      <input type="submit" />
+    </form>
+  );
+}
