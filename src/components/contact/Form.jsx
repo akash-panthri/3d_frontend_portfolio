abd94eb3e5abe9114f1f616f14ccb5501130b18a
akash-panthri <akashpanthri2000@gmail.com>
2024-12-08 07:24:12 +0530

    Styled input elements in form.jsx

--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -1,17 +1,38 @@
-"use client"
-import React from 'react';
-import { useForm } from 'react-hook-form';
+"use client";
+import React from "react";
+import { useForm } from "react-hook-form";
 
 export default function Form() {
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
   console.log(errors);
-  
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="text" placeholder="name" {...register("name", {required: true})} />
-      <input type="email" placeholder="email" {...register("email", {required: true})} />
-      <textarea {...register("message", {required: true, max: 256, min: 50})} />
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="max-w-md w-full flex flex-col items-center justify-center space-y-4"
+    >
+      <input
+        type="text"
+        placeholder="name"
+        {...register("name", { required: true })}
+        className="w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg"
+      />
+      <input
+        type="email"
+        placeholder="email"
+        {...register("email", { required: true })}
+        className="w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg"
+      />
+      <textarea
+      placeholder="message"
+        {...register("message", { required: true, max: 256, min: 50 })}
+        className="w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg"
+      />
 
       <input type="submit" />
     </form>
