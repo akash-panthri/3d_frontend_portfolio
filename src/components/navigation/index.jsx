44a21e72c1bd5463e162253b6b5a82f56199ea1d
akash-panthri <akashpanthri2000@gmail.com>
2024-12-12 07:59:38 +0530

    Added xs screen in tailwind.config.js

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -31,7 +31,20 @@ export default function Navigation() {
         })}
       </div>) : (
         <>
-          <h1>Navigation Response</h1>
+        <div className="w-max flex items-center justify-center group hover:pause relative animate-spin-slow">
+        {BtnList.map((btn, index) => {
+          const angleRad = (index * angleIncrement * Math.PI) / 180;
+
+          const radius = isLarge
+            ? "calc(20vw - 1rem)"
+            : isMedium
+            ? "calc(30vw - 1rem)"
+            : "calc(40vw - 1rem)";
+          const x = `calc(${radius}*${Math.cos(angleRad)})`;
+          const y = `calc(${radius}*${Math.sin(angleRad)})`;
+          return <NavButton key={index} x={x} y={y} {...btn} />;
+        })}
+      </div>
         </>
       );
     }}
