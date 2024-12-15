5202d417c5c1699667f24d6ea164742f0639fc4c
akash-panthri <akashpanthri2000@gmail.com>
2024-12-15 07:08:21 +0530

    Made project responsive

--- src/app/(sub-folder)/projects/page.js
+++ src/app/(sub-folder)/projects/page.js
@@ -14,7 +14,7 @@ export default function Home() {
         className="-z-50 w-full h-full object-cover object-center opacity-25 fixed top-20 left-0"
       />
       <ProjectList projects={projectsData} />
-      <div className="flex items-center justify-center fixed  top-20 -left-24 h-screen">
+      <div className="flex items-center justify-center fixed  top-16 lg:top-20 -translate-x-1/2 lg:translate-x-0 left-1/2 lg:-left-24 h-screen -z-10">
         <RenderModel>
           <Staff />
         </RenderModel>
