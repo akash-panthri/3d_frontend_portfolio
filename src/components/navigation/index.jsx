0c66c557b2ea3fd925a2e7267f97446cbe52267e
akash-panthri <akashpanthri2000@gmail.com>
2024-12-16 07:05:34 +0530

    Added framer motion div in navigation/index

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -4,6 +4,7 @@ import { BtnList } from "@/app/data";
 import NavButton from "./NavButton";
 import useScreenSize from "../hooks/useScreenSize";
 import ResponsiveComponent from "../ResponsiveComponent";
+import { motion } from "framer-motion";
 
 export default function Navigation() {
   const angleIncrement = 360 / BtnList.length;
@@ -15,7 +16,7 @@ export default function Navigation() {
       <ResponsiveComponent>
         {({ size }) => {
           return size && size >= 480 ? (
-            <div className="w-max flex items-center justify-center group hover:pause relative animate-spin-slow">
+            <motion.div className="w-max flex items-center justify-center group hover:pause relative animate-spin-slow">
               {BtnList.map((btn, index) => {
                 const angleRad = (index * angleIncrement * Math.PI) / 180;
 
@@ -28,20 +29,26 @@ export default function Navigation() {
                 const y = `calc(${radius}*${Math.sin(angleRad)})`;
                 return <NavButton key={index} x={x} y={y} {...btn} />;
               })}
-            </div>
+            </motion.div>
           ) : (
             <>
               <div className="w-full px-2.5 xs:p-0 xs:w-max flex flex-col items-start xs:items-center space-y-4 justify-center group relative">
                 {BtnList.slice(0, BtnList.length / 2).map((btn, index) => {
-                  
                   return <NavButton key={index} x={0} y={0} {...btn} />;
                 })}
               </div>
               <div className="w-full px-2.5 xs:p-0 xs:w-max flex flex-col items-end xs:items-center justify-center space-y-4 group relative">
                 {BtnList.slice(BtnList.length / 2, BtnList.length).map(
                   (btn, index) => {
-                    
-                    return <NavButton key={index} x={0} y={0} {...btn} labelDirection="left" />;
+                    return (
+                      <NavButton
+                        key={index}
+                        x={0}
+                        y={0}
+                        {...btn}
+                        labelDirection="left"
+                      />
+                    );
                   }
                 )}
               </div>
