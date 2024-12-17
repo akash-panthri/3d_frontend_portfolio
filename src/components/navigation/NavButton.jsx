64eb8335a58850586c1a1f6cab805e711293b93f
akash-panthri <akashpanthri2000@gmail.com>
2024-12-17 07:18:39 +0530

    Added item attribute in Link in navigation/NavButton.jsx

--- src/components/navigation/NavButton.jsx
+++ src/components/navigation/NavButton.jsx
@@ -60,6 +60,7 @@ function NavButton({
             style={{ transform: `translate(${x}, ${y})` }}
           >
             <Link
+            variants={item}
               href={link}
               target={newTab ? "_blank" : "_self"}
               className="text-foreground  rounded-full flex items-center justify-center bg-background/20 border border-accent/30 border-solid backdrop-blur-[6px] shadow-glass-inset hover:shadow-glass-sm"
