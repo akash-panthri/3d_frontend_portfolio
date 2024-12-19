1396df2a74aaf0e879ab6ac81e1be4f016b6c58a
akash-panthri <akashpanthri2000@gmail.com>
2024-12-19 07:08:50 +0530

    Added container and item constant in Form.jsx

--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -4,6 +4,21 @@ import emailjs from "@emailjs/browser";
 import { useForm } from "react-hook-form";
 import { Toaster, toast } from "sonner";
 
+const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.3,
+    },
+  },
+};
+
+const item = {
+  hidden: { scale: 0 },
+  show: { scale: 1 },
+};
+
 export default function Form() {
   const {
     register,
